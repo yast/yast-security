6f78e7da615deb7e474265e7470f5a2c35d570c6
Stefan Schubert <schubi@suse.de>
2019-09-30 16:42:37 +0200

    Master bsc 1150821 (#58)
    * adapt Rakefile and Dockerfile for SLE-15-SP1
    
    * Sle 15 sp1 bsc 1147173 (#55)
    
    * Supporting user defined permission file in AY
    
    * Sle 15 sp1 bsc 1150821 (#57)
    
    * console_shutdown entry has not been evaluated correctly
    
    * merged with SLES15-SP1

--- src/modules/Security.rb
+++ src/modules/Security.rb
@@ -314,13 +314,7 @@ module Yast
     # Read the information about ctrl+alt+del behavior
     # See bug 742783 for description
     def ReadConsoleShutdown
-      ret = ::Security::CtrlAltDelConfig.current || ::Security::CtrlAltDelConfig.default
-
-      return ret if ::Security::CtrlAltDelConfig.systemd?
-
-      @Settings["CONSOLE_SHUTDOWN"] = ret if ::Security::CtrlAltDelConfig.inittab?
-
-      nil
+     @Settings["CONSOLE_SHUTDOWN"] = ::Security::CtrlAltDelConfig.current || ::Security::CtrlAltDelConfig.default   
     end
 
     # Read the settings from the files included in @Locations
@@ -435,7 +429,7 @@ module Yast
       # Read security settings
       read_from_locations
 
-      @Settings["CONSOLE_SHUTDOWN"] = ReadConsoleShutdown()
+      ReadConsoleShutdown()
 
       log.debug "Settings (after read console shutdown): #{@Settings}"
 
@@ -785,7 +779,6 @@ module Yast
     publish :function => :SetModified, :type => "void ()"
     publish :function => :Modified, :type => "boolean ()"
     publish :function => :ReadServiceSettings, :type => "void ()"
-    publish :function => :ReadConsoleShutdown, :type => "string ()"
     publish :function => :Read, :type => "boolean ()"
     publish :function => :Write, :type => "boolean ()"
     publish :function => :Import, :type => "boolean (map)"
