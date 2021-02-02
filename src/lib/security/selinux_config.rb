636295410db427571e220483d8606e8a3cf956f6
David Díaz González <dgonzalez@suse.de>
2021-02-02 10:17:48 +0000

    WIP: Move on, improving the SelinuxConfig class
    Also adds unit tests and documentation

--- src/lib/security/selinux_config.rb
+++ src/lib/security/selinux_config.rb
@@ -25,15 +25,35 @@ module Security
   class SelinuxConfig
     include Yast::Logger
     Yast.import "Bootloader"
+    Yast.import "ProductFeatures"
 
     attr_reader :policy
 
+    # Constructor
     def initialize
-      propose_policy if Yast::Mode.installation
-      @policy = configured_policy
+      @policy = initial_policy
+    end
+
+    # Return the initial SELinux policy
+    #
+    # It can be the proposed one if running in an installation or the configured policy for a
+    # running system
+    #
+    # @return [Symbol]
+    def initial_policy
+      @initial_policy ||=
+        begin
+          propose_policy if Yast::Mode.installation
+          configured_policy
+        end
     end
 
     # Returns the policy applied in the running system
+    #
+    # @note the system can be booted with a different SELinux policy that the configured one. To
+    #  know the running policy getenforce tool is used.
+    #
+    # @return [Symbol] running SELinux policy if command executed successfully; :disabled otherwise
     def running_policy
       Yast::Execute.locally!(GETENFORCE_PATH, stdout: :capture).chomp.downcase.to_sym
     rescue Cheetah::ExecutionFailed
@@ -41,76 +61,120 @@ module Security
       :disabled
     end
 
-    # If current policy value is different to the configured one
-    def changed?
-      @policy != configured_policy
+    # Set the policy to given value
+    #
+    # @note using nil means to set SELinux policy as disabled.
+    #
+    # @param id [String, Symbol, nil] a SELinux policy identifier
+    # @return [] true when policy found and set; false otherwise
+    def policy=(id)
+      @policy = id&.to_sym || :disabled
     end
 
-    # Use the given policy for the next boot (in running system is needed to #save)
-    def update_policy(policy_key)
-      found_policy = find_policy(policy_key)
-
-      if found_policy
-        log.info("Changing SELinux configuration to `#{policy_key}` policy: #{found_policy}")
-
-        @policy = policy_key
-      else
-        log.info("Unknown `#{policy_key}` SELinux policy")
-      end
-    end
-    alias_method :policy=, :update_policy
-
+    # Set current policy options as kernel parameters for the next boot
+    #
+    # @note it does not write the changes when running in installation mode, where only sets the
+    #   kernel params in memory, since the Yast::Bootloader.Write will be performed at the end of
+    #   installation.
+    #
+    # @see Yast::Bootloader#modify_kernel_params
+    #
+    # @return [Boolean] false if there area not policy options or nothing changed; true otherwise
     def save
       policy_options = find_policy(policy)
 
-      return unless policy_options && changed?
+      unless policy_options
+        log.info("Unknown `#{policy}` SELinux policy")
+        return false
+      end
 
-      log.info("Writting SELinux kernel params: #{policy_options}")
-
-      Yast::Bootloader.modify_kernel_params(**policy_options)
-      Yast::Bootloader.Write unless Yast::Mode.installation
+      changed = Yast::Bootloader.modify_kernel_params(**policy_options)
+      changed = Yast::Bootloader.Write if changed && !Yast::Mode.installation
+      changed
     end
 
     private
 
+    # Path to the SELinux getenforce command
     GETENFORCE_PATH = "/usr/sbin/getenforce".freeze
     private_constant :GETENFORCE_PATH
 
+    # Known keys for setting a SELinux policy via kernel command line
     POLICY_KEYS = ["security", "selinux", "enforcing"].freeze
     private_constant :POLICY_KEYS
 
+    # Known SELinux policies
+    #
+    # This is _the main_ or _base_ configuration for known SELinux policies. However, note that, for
+    # example, a permissive mode could be set by just setting the "security" module; i.e.,
+    # "security=selinux" means "enable SELinux using the permissive mode". Or even setting
+    # the enforcing param to a value equal or less than 0; i.e., "security=selinux enforcing=0".
+    #
+    # Additionally, removing the "security" from the kernel params does not mean to use none
+    # security module. Instead, it just fallback to the kernel configuration at the compile time,
+    # which in SUSE is to use AppArmor according to the CONFIG_LSM variable. So, it could be said
+    # that dropping all policy param to disabling SELinux is safe enough.
+    #
+    # To know more, please visit the LSM Usage documentation at
+    # https://www.kernel.org/doc/html/latest/admin-guide/LSM/index.html
+    # and/or grep for CONFIG_LSM in /boot/config-*
     POLICIES = {
+      # NOTE: setting the "security" module to :missing, which means to remove the options from the
+      #       kernel
       disabled:   { "security" => :missing, "selinux" => :missing, "enforcing" => :missing },
       permissive: { "security" => "selinux", "selinux" => "1", "enforcing" => :missing },
       enforcing:  { "security" => "selinux", "selinux" => "1", "enforcing" => "1" },
     }.freeze
     private_constant :POLICIES
 
-    def configured_policy
-      match_policy(policy_options_from_kernel) || :disabled
+    # Returns the requested policy, if exists
+    #
+    # @param key [String, Symbol] the policy identifier
+    # @return [Hash, nil] options for matched policy or nil if none
+    def find_policy(key)
+      POLICIES[key.to_sym]
     end
 
-    # Propose a policy according to the value set in the control-file
+    # Proposes a policy based on `selinux_policy` value set in the control file
     def propose_policy
-      key = :enforcing # read it from control-file
+      policy_key = Yast::ProductFeatures.GetFeature("globals", "selinux_policy").to_sym
 
-      log.info "Proposing the `#{key}` SELinux policy"
-      update_policy(policy_key)
+      log.info "Proposing the `#{policy_key}` SELinux policy"
+      self.policy = policy_key
       save
     end
 
-    def find_policy(key)
-      POLICIES[key]
+    # Returns the configured SELinux policy according to params in kernel command line
+    #
+    # @see #policy_from_kernel_params
+    #
+    # @return [Symbol] the policy identifier
+    def configured_policy
+      options = policy_from_kernel_params
+
+      return :disabled if options.empty?
+
+      security_module = options["security"]
+      module_disabled = options["selinux"].to_i <= 0
+      enforcing_mode  = options["enforcing"].to_i > 0
+
+      return :disabled if security_module != "selinux" || module_disabled
+      return :permissive unless enforcing_mode
+
+      :enforcing
     end
 
-    def match_policy(policy_options)
-      POLICIES.key(policy_options)
+    # Returns the SELinux configuration based on options set in the kernel command line
+    #
+    # @return [Symbol] the policy identifier
+    def policy_from_kernel_params
+      options = Hash[*POLICY_KEYS.flat_map { |key| [key, read_param(key)] }]
+      options.filter { |_, value| value != :missing }
     end
 
-    def policy_options_from_kernel
-      Hash[*POLICY_KEYS.flat_map { |key| [key, read_param(key)] }]
-    end
-
+    # Convenience method to read a value from kernel parameters
+    #
+    # @see Yast::Bootloader#kernel_param
     def read_param(key)
       Yast::Bootloader.kernel_param(:common, key.to_s)
     end
--- test/security/selinux_config_test.rb
+++ test/security/selinux_config_test.rb
@@ -24,25 +24,222 @@ require "security/selinux_config"
 
 describe Security::SelinuxConfig do
   subject { described_class.new }
+  let(:installation_mode) { false }
+  let(:default_policy) { "enforcing" }
 
   before do
-    allow(Yast::Bootloader).to receive(:kernel_param).with(:common, "security")
-      .and_return(security)
-    allow(Yast::Bootloader).to receive(:kernel_param).with(:common, "selinux")
-      .and_return(selinux)
-    allow(Yast::Bootloader).to receive(:kernel_param).with(:common, "enforcing")
-      .and_return(enforcing)
+    allow(Yast::Mode).to receive(:installation).and_return(installation_mode)
+    allow(Yast::ProductFeatures).to receive(:GetFeature).with("globals", "selinux_policy")
+      .and_return(default_policy)
+  end
+
+  RSpec.shared_examples 'initial policy' do
+    context "in a running system" do
+      before do
+        allow(Yast::Bootloader).to receive(:kernel_param).with(:common, "security")
+          .and_return(security_module)
+        allow(Yast::Bootloader).to receive(:kernel_param).with(:common, "selinux")
+          .and_return(selinux_status)
+        allow(Yast::Bootloader).to receive(:kernel_param).with(:common, "enforcing")
+          .and_return(selinux_enforcing)
+      end
+
+      context "with a SELinux configuration" do
+        let(:security_module) { "selinux" }
+        let(:selinux_status) { "1" }
+        let(:selinux_enforcing) { "0" }
+
+        it "returns the configured policy" do
+          expect(subject.initial_policy).to eq(:permissive)
+        end
+      end
+
+      context "without a SELinux configuration" do
+        let(:security_module) { "apparmor" }
+        let(:selinux_status) { :missing }
+        let(:selinux_enforcing) { :missing }
+
+        it "returns :disabled" do
+          expect(subject.initial_policy).to eq(:disabled)
+        end
+      end
+    end
+
+    context "during installation" do
+      let(:installation_mode) { true }
+
+      it "returns the default proposed policy" do
+        expect(subject.initial_policy).to eq(default_policy.to_sym)
+      end
+    end
+  end
+
+  describe "initial_policy" do
+    include_examples "initial policy"
   end
 
   describe "#policy" do
-    pending
+    context "when policy has not been changed yet" do
+      include_examples "initial policy"
+    end
+
+    context "when policy has been changed" do
+      before do
+        subject.policy = :disabled
+      end
+
+      context "in a running system" do
+        it "returns the chosen policy" do
+          expect(subject.policy).to eq(:disabled)
+        end
+      end
+
+      context "during the installation" do
+        let(:installation_mode) { true }
+
+        it "returns the chosen policy" do
+          expect(subject.policy).to eq(:disabled)
+        end
+      end
+    end
+
   end
 
   describe "#policy=" do
-    pending
+    context "when a symbol or string given" do
+      it "sets it as a current policy" do
+        subject.policy = :permissive
+        expect(subject.policy).to eq(:permissive)
+
+        subject.policy = "whatever"
+        expect(subject.policy).to eq(:whatever)
+      end
+    end
+
+    context "when nil is given" do
+      it "sets current policy as :disabled" do
+        subject.policy = nil
+        expect(subject.policy).to eq(:disabled)
+      end
+    end
   end
 
   describe "#running_policy=" do
-    pending
+    let(:getenforce_cmd) { ["/usr/sbin/getenforce", stdout: :capture] }
+    let(:cheetah_error) { Cheetah::ExecutionFailed.new([], "", nil, nil) }
+
+    context "when getenforce tool is available" do
+      before do
+        allow(Yast::Execute).to receive(:locally!).with(*getenforce_cmd).and_return("Enforcing\n")
+      end
+
+      it "returns its sanitize output as a symbol" do
+        expect(subject.running_policy).to eq(:enforcing)
+      end
+    end
+
+    context "when getenforce tool is not available" do
+      before do
+        allow(Yast::Execute).to receive(:locally!).with(*getenforce_cmd)
+          .and_raise(cheetah_error)
+      end
+
+      it "logs a debug message" do
+        expect(subject.log).to receive(:debug).with(/.*getenforce.*not available/)
+
+        subject.running_policy
+      end
+
+      it "returns :disabled" do
+        expect(subject.running_policy).to eq(:disabled)
+      end
+    end
+  end
+
+  describe "#save" do
+    before do
+      allow(Yast::Bootloader).to receive(:Write).and_return(true)
+    end
+
+    RSpec.shared_examples "does not change the policy" do
+    end
+
+    context "when working with a known policy" do
+      context "that is already set" do
+        before do
+          subject.policy = :permissive
+          subject.save
+          subject.policy = :permissive
+        end
+
+        it "returns false" do
+          expect(subject.save).to eq(false)
+        end
+
+        it "does not write bootloader configuration" do
+          expect(Yast::Bootloader).to_not receive(:Write)
+
+          subject.save
+        end
+      end
+
+      context "that is not set yet" do
+        before do
+          subject.policy = :permissive
+          subject.save
+          subject.policy = :disabled
+        end
+
+        it "returns true" do
+          expect(subject.save).to eq(true)
+        end
+
+        it "modifies kernel params" do
+          expect(Yast::Bootloader).to receive(:modify_kernel_params)
+
+          subject.save
+        end
+
+        context "in a running system" do
+          it "writes bootloader configuration" do
+            expect(Yast::Bootloader).to receive(:Write)
+
+            subject.save
+          end
+        end
+
+        context "during installation" do
+          let(:installation_mode) { true }
+
+          it "does not write bootloader configuration" do
+            expect(Yast::Bootloader).to_not receive(:Write)
+
+            subject.save
+          end
+        end
+      end
+    end
+
+    context "when set policy is unknonw" do
+      before do
+        subject.policy = :unknown
+      end
+
+      it "returns false" do
+        expect(subject.save).to eq(false)
+      end
+
+      it "does not modify kernel params" do
+        expect(Yast::Bootloader).to_not receive(:modify_kernel_params)
+
+        subject.save
+      end
+
+      it "does not write bootloader configuration" do
+        expect(Yast::Bootloader).to_not receive(:Write)
+
+        subject.save
+      end
+    end
   end
 end
