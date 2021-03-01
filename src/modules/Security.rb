dd07b8d6538800ac2450466bd01e5a249bfe7f11
David Díaz González <dgonzalez@suse.de>
2021-03-01 11:45:28 +0000

    Add explanatory comment

--- src/modules/Security.rb
+++ src/modules/Security.rb
@@ -903,6 +903,8 @@ module Yast
     def set_selinux_patterns
       selinux_config.mode = @Settings["SELINUX_MODE"] unless @Settings["SELINUX_MODE"].to_s.empty?
 
+      # Please, keep the unique id synced with the one used in normal installation
+      # See https://github.com/yast/yast-firewall/blob/c3ae49a1009dbf324fa3558dff6c5e147c495268/src/lib/y2firewall/clients/proposal.rb#L229-L230
       PackagesProposal.SetResolvables("SELinux", :pattern, selinux_config.needed_patterns)
     end
 
