caaafe23542da595c79ed863bf9411678c9241dd
Imobach González Sosa <igonzalezsosa@suse.com>
2022-08-04 14:07:36 +0100

    Add a basic security policy proposal

---
+++ src/lib/y2security/clients/security_policy_proposal.rb
@@ -0,0 +1,111 @@
+# Copyright (c) [2022] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+#
+require "installation/proposal_client"
+require "y2security/security_policy"
+
+module Y2Security
+  module Clients
+    class SecurityPolicyProposal < ::Installation::ProposalClient
+      include Yast::I18n
+      include Yast::Logger
+
+      LINKS = [
+        LINK_ENABLE = "security-policy--enable".freeze,
+        LINK_DISABLE = "security-policy--disable".freeze,
+      ].freeze
+
+      LINK_DIALOG = "security_policy".freeze
+
+      def initialize
+        super
+        Yast.import "UI"
+        Yast.import "HTML"
+        textdomain "security"
+      end
+
+      def description
+        {
+          # Proposal title
+          "rich_text_title" => _("Security Policy"),
+          # Menu entry label
+          "menu_title"      => _("&Security Policy"),
+          "id"              => LINK_DIALOG
+        }
+      end
+
+      def make_proposal(_attrs)
+        {  
+          "preformatted_proposal" => preformatted_proposal,
+          "warning_level" => warning_level,
+          "links" => LINKS,
+          "warning" => warning_message
+        }
+      end
+
+      def preformatted_proposal
+        if stig_policy.enabled?
+          _(
+            "STIG is enabled (<a href=\"%s\">disable</a>)",
+          ) % LINK_DISABLE
+        else
+          _(
+            "STIG is not enabled (<a href=\"%s\">enable</a>)",
+          ) % LINK_ENABLE
+        end
+      end
+
+      def ask_user(param)
+        chosen_link = param["chosen_id"]
+        case chosen_link
+        when LINK_DISABLE
+          stig_policy.disable
+        when LINK_ENABLE
+          stig_policy.enable
+        end
+
+        { "workflow_result" => :again }
+      end
+
+    private
+
+      def enable_stig
+        stig_policy.enable
+      end
+
+      def disable_stig
+        stig_policy.disable
+      end
+
+      def warning_message
+        return nil if !stig_policy.enabled? || stig_policy.valid?
+
+        Yast::HTML.List(stig_policy.errors)
+      end
+
+      def warning_level
+        :warning
+      end
+
+      def stig_policy
+        @stig_policy ||= Y2Security::SecurityPolicy.find(:stig)
+      end
+    end
+  end
+end
